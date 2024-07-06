2052ae55063be6dec31801a1d6a4f38f40e193ee
Luiz-Lennon <lennonsjm1@gmail.com>
2024-07-06 10:34:23 -0300

    atualização_06/07/24

---
+++ aulas/Dicionario Criação e acesso a dados/Métodos da classe dict/del.py
@@ -0,0 +1,14 @@
+contatos = {
+    "guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"},
+    "giovanna@gmail.com": {"nome": "Giovanna", "telefone": "3443-2121"},
+    "chappie@gmail.com": {"nome": "Chappie", "telefone": "3344-9871"},
+    "melaine@gmail.com": {"nome": "Melaine", "telefone": "3333-7766"},
+}
+
+del contatos["guilherme@gmail.com"]["telefone"]
+del contatos["chappie@gmail.com"] 
+
+contatos # {'guilherme@gmail.com': {'nome': 'Guilherme'}, 'giovanna@gmail.com': {'nome': 'Giovanna', 'telefone': '3443-2121'}, 'melaine@gmail.com': {'nome': 'Melaine', 'telefone': '3333-7766'}}
+
+# É uma maneira também para remover tanto tudo colocando del e o nome da biblioteca
+# E pode remover algo especifico como no exemplo de cima onde só tirou o telefone
